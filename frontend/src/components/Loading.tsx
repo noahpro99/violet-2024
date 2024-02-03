56105a1720b0e242d42e55e9c05068bed646808a
noahpro <noahpro@gmail.com>
2024-02-03 18:32:49 -0500

    ratings page and dash

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -8,12 +8,12 @@ type Props = {
 
 export default function Loading({ spinner, text }: Props) {
   return (
-    <div className="flex flex-col items-center min-h-screen bg-[#f7d2ce]">
+    <div className="flex flex-col items-center min-h-screen bg-white">
       {/* pink spinner */}
       {spinner && <div className="w-11/12 h-96 flex justify-center items-center">
-        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-[#f6a4ab]"></div>
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500"></div>
       </div>}
-      {text && <div className="text-2xl font-bold text-gray-500">{text}</div>}
+      {text && <div className="text-2xl font-bold text-black">{text}</div>}
 
       <Navbar />
 
--- frontend/src/pages/Result.tsx
+++ frontend/src/pages/Result.tsx
@@ -10,11 +10,20 @@ import Loading from '../components/Loading';
 const TestResultCard: React.FC<{ result: InputResultOutcome, label: string }> = ({ result, label }) => {
 
   return (
-    <div className='p-4 w-full'>
-      <div className='flex flex-col w-full items-center justify-between py-2 border-2 border-[#f6a4ab] rounded-2xl px-4'>
-        <div className='flex justify-between w-full'>
+    <div className='p-2 w-full'>
+      <div className='flex flex-col w-full items-center justify-between py-2 border-2 border-blue-500 rounded-2xl px-4'>
+        <div className='flex justify-between w-full items-center'>
+          {/* svg up arrow */}
           <div className='text-lg font-semibold'>{label}</div>
-          <div className={`text-lg font-semibold ${result === InputResultOutcome.HIGH ? 'text-red-500' : 'text-green-500'}`}>{result}</div>
+
+          {result === InputResultOutcome.HIGH && <svg className="w-6 h-6 text-red-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M5 10l7-7m0 0l7 7m-7-7v18" />
+          </svg>}
+          {result === InputResultOutcome.LOW && <svg className={`w-3 h-6 text-red-500`} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
+          </svg>}
+          {/* green dot for normal */}
+          {result === InputResultOutcome.NORMAL && <div className='w-3 h-3 mx-1 bg-green-500 rounded-full'></div>}
         </div>
       </div>
     </div>
@@ -53,24 +62,47 @@ export default function Result() {
     return <Loading text="Record not found" />
   }
 
+  let resultIntToStr = [
+    "Low",
+    "Medium",
+    "High"
+  ]
+
+
   return (
-    <div className="flex flex-col items-center min-h-screen bg-[#f7d2ce]">
+    <div className="flex flex-col items-center min-h-screen bg-[white]">
       <div className="w-11/12 ">
         {record.result === 0 && <Fireworks onInit={onInit} />}
         <div className="text-center text-2xl font-bold mb-4 text-">Test Results</div>
         <div className="text-center mt-8">
           <div className="date text-lg">{record.date}</div>
           <div className={`risk-result text-xl font-semibold ${record.result === 2 ? 'text-red-500' : 'text-green-500'}`}>
-            Risk: {record.result}
+            Risk: {resultIntToStr[record.result]}
           </div>
         </div>
-        {record.res_age !== InputResultOutcome.NORMAL && <TestResultCard result={record.res_age} label="Age" />}
-        {record.res_systolic_bp !== InputResultOutcome.NORMAL && <TestResultCard result={record.res_systolic_bp} label="Systolic BP" />}
-        {record.res_diastolic_bp !== InputResultOutcome.NORMAL && <TestResultCard result={record.res_diastolic_bp} label="Diastolic BP" />}
-        {record.res_bs !== InputResultOutcome.NORMAL && <TestResultCard result={record.res_bs} label="Blood Sugar" />}
-        {record.res_body_temp !== InputResultOutcome.NORMAL && <TestResultCard result={record.res_body_temp} label="Body Temperature" />}
-        {record.res_heart_rate !== InputResultOutcome.NORMAL && <TestResultCard result={record.res_heart_rate} label="Heart Rate" />}
+        <TestResultCard result={record.res_age} label="Age" />
+        <TestResultCard result={record.res_systolic_bp} label="Systolic BP" />
+        <TestResultCard result={record.res_diastolic_bp} label="Diastolic BP" />
+        <TestResultCard result={record.res_bs} label="Blood Sugar" />
+        <TestResultCard result={record.res_body_temp} label="Body Temperature" />
+        <TestResultCard result={record.res_heart_rate} label="Heart Rate" />
+
+        {/* put response */}
+        <div className='p-3 w-full'>
+          <div className='flex flex-col w-full items-center justify-between py-2 border-2 border-blue-500 rounded-2xl px-4'>
+            <div className="text-center text-2xl font-bold mb-4">Recommendations</div>
+            {record.recommendation.split('\n').map((line, index) => {
+              return (
+                <div>
+                  <p key={index} className="text-center">{line}</p>
+                  <br />
+                </div>
+              )
+            })}
+          </div>
+        </div>
       </div>
+      <div className='py-10'></div>
 
       <Navbar />
     </div >
