2bb3c410f3eabae9fa7d787e1d7e279945d0e37f
noahpro <noahpro@gmail.com>
2024-02-03 17:58:26 -0500

    date picker

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,4 +1,3 @@
-// Login.tsx
 import React, { useState } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
@@ -23,28 +22,66 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold mb-4">Login</h1>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        className="border border-gray-400 rounded-lg px-4 py-2 mb-4 w-80"
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        className="border border-gray-400 rounded-lg px-4 py-2 mb-4 w-80"
-      />
-      <button onClick={handleLogin} className="bg-blue-500 text-white rounded-lg px-4 py-2">
-        Login
-      </button>
-      {/* signup link */}
-      <a href="/signup">Signup</a>
-    </div>
+    <>
+      <div className="flex min-h-screen flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 className="p-2 text-center text-2xl font-bold leading-9 tracking-tight text-blue-500">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="p-2 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white px-6 py-6 shadow-lg rounded-2xl sm:px-12 border-2 border-blue-500" style={{ boxShadow: '0 10px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)' }}>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </label>
+              <div className="p-2">
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="block w-full rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                Password
+              </label>
+              <div className="p-2">
+                <input
+                  type="password"
+                  placeholder="Enter your password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="block w-full rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className='p-2'>
+              <button
+                type="submit"
+                onClick={handleLogin}
+                className="flex w-full justify-center rounded-md bg-blue-500 px-4 py-2 text-sm font-medium leading-6 text-white shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              >
+                Sign in
+              </button>
+            </div>
+          </div>
+
+          <p className="p-2 text-center text-sm text-gray-500">
+            Don't have an account?{' '}
+            <a href="/signup" className="font-medium text-blue-600 hover:underline">
+              Create account
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
 
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -2,58 +2,142 @@ import React, { useState } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import { DefaultService } from '../api';
+import { Popover, PopoverContent, PopoverTrigger } from '../components/ui/popover';
+import { format } from "date-fns"
+import { CalendarIcon } from 'lucide-react';
+import { Button } from '../components/ui/button';
+import { cn } from '../lib/utils';
+import { Calendar } from '../components/ui/calendar';
 
 const Signup: React.FC = () => {
   const { login } = useAuth();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [username, setUsername] = useState('');
+  const [date, setDate] = useState<Date>();
   const navigate = useNavigate();
 
-  const handleSignup = async () => {
-    // DefaultService.signupSignupPost({
-    //   email: email,
-    //   password: password,
-    //   username: username
-    // }).then((response) => {
-    //   login(response.token);
-    //   navigate('/dashboard');
-    // }).catch((error) => {
-    //   console.log(error);
-    // });
+  const handleSignup = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (!email || !password || !username || !date) {
+      return;
+    }
+    DefaultService.signupSignupPost({
+      email: email,
+      password: password,
+      username: username,
+      birth_date: date.toISOString().split('T')[0],
+    }).then((response) => {
+      login(response.token);
+      navigate('/dashboard');
+    }).catch((error) => {
+      console.log(error);
+    });
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold mb-4">Login</h1>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        className="border border-gray-400 rounded-lg px-4 py-2 mb-4 w-80"
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        className="border border-gray-400 rounded-lg px-4 py-2 mb-4 w-80"
-      />
-      <input
-        type="name"
-        placeholder="Name"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        className="border border-gray-400 rounded-lg px-4 py-2 mb-4 w-80"
-      />
-      <button onClick={handleSignup} className="bg-blue-500 text-white rounded-lg px-4 py-2">
-        Signup
-      </button>
-      {/* a;ready have account, login */}
-      <a href="/login">Login</a>
+    <>
+      <div className="flex min-h-screen flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 className="p-2 text-center text-2xl font-bold leading-9 tracking-tight text-blue-500">
+            Sign in to your account
+          </h2>
+        </div>
 
-    </div>
+        <div className="p-2 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white px-6 py-6 shadow-lg rounded-2xl sm:px-12 border-2 border-blue-500" style={{ boxShadow: '0 10px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)' }}>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </label>
+              <div className="p-2">
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="block w-full rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                Password
+              </label>
+              <div className="p-2">
+                <input
+                  type="password"
+                  placeholder="Enter your password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="block w-full rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+                Name
+              </label>
+              <div className="p-2">
+                <input
+                  placeholder="Enter your name"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  className="block w-full rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="birthday" className="block text-sm font-medium leading-6 text-gray-900">
+                Birthday
+              </label>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant={"outline"}
+                    className={cn(
+                      "w-[280px] justify-start text-left font-normal",
+                      !date && "text-muted-foreground"
+                    )}
+                  >
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {date ? format(date, "yyyy-mm-dd") : <span>Pick a date</span>}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0">
+                  <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+            </div>
+
+            <div className='p-2'>
+              <button
+                type="submit"
+                onClick={handleSignup}
+                className="flex w-full justify-center rounded-md bg-blue-500 px-4 py-2 text-sm font-medium leading-6 text-white shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              >
+                Sign up
+              </button>
+            </div>
+          </div>
+
+          <p className="p-2 text-center text-sm text-gray-500">
+            Already have an account?{' '}
+            <a href="/login" className="font-medium text-blue-600 hover:underline">
+              Login
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
 
