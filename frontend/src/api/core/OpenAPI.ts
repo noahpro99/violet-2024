7c4c6eb061e20aa98d862fb2943777b47b6d13bc
noahpro <noahpro@gmail.com>
2024-02-04 09:09:19 -0500

    point to real backend

--- frontend/src/api/core/OpenAPI.ts
+++ frontend/src/api/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: 'http://localhost:8080',
+    BASE: 'https://backend-ozse45n24a-ue.a.run.app',
     VERSION: '0.1.0',
     WITH_CREDENTIALS: false,
     CREDENTIALS: 'include',
